52aadf5613c5fdee87bbb2e97224283506d8071d
vertina61 <natalya.vertieva@mail.ru>
2022-06-11 15:26:17 +0300

    Использовали тернарный оператор в функциях

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ const returnRandomInteger = function (min, max) {
   min=Math.ceil(min);
   max=Math.floor(max);
   const rez = Math.floor(Math.random() * (max - min + 1)) + min;
-  if(min>0) {return `${ rez } целое число из диапазона от ${ min } до ${ max }`;}
-  return('Упс, число не может быть меньше или равно 0');
+  return(min>0) ? `${ rez } целое число из диапазона от ${ min } до ${ max }`:'Упс, число не может быть меньше или равно 0';
 };
 returnRandomInteger(1,78);
 
