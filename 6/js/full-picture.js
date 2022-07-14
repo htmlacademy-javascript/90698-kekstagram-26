ace0c87213f22a942fd9cb88e8a56602e9ea39fe
Keks <keks@htmlacademy.ru>
2022-07-14 13:12:35 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/full-picture.js
@@ -0,0 +1,48 @@
+import { isEscapeKey } from './util.js';
+const openFullElement=document.querySelector('.big-picture');
+const closeСommentsElement=document.querySelector('.social__comment-count');
+const loadNewCommentsElement=document.querySelector('.comments-loader');
+const bodyElement=document.querySelector('body');
+const closeBigPictureElement=document.querySelector('.big-picture__cancel');
+const fullPictureImageElement=document.querySelector('.big-picture__img img');
+
+const renderFullPicture = (({url, likes, comments, description}) => {
+  fullPictureImageElement.src = url;
+  openFullElement.querySelector('.likes-count').textContent = likes;
+  openFullElement.querySelector('.comments-count').textContent = comments;
+  openFullElement.querySelector('.comments-count').textContent = comments.length;
+  openFullElement.querySelector('.social__caption').textContent = description;
+  openFullPicture();
+});
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFullPicture();
+  }
+};
+
+function openFullPicture() {
+  openFullElement.classList.remove('hidden');
+  closeСommentsElement.classList.add('hidden');
+  loadNewCommentsElement.classList.add('hidden');
+  bodyElement.classList.add('modal-open');
+
+  document.addEventListener('keydown', onPopupEscKeydown);
+
+}
+
+function closeFullPicture() {
+  openFullElement.classList.add('hidden');
+  closeСommentsElement.classList.remove('hidden');
+  loadNewCommentsElement.classList.remove('hidden');
+  bodyElement.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
+
+closeBigPictureElement.addEventListener('click',()=>{
+  closeFullPicture();
+});
+
+export{renderFullPicture};
