6cd6b4215c24e22dfab1cec4dfa69686aea4bc56
Keks <keks@htmlacademy.ru>
2022-07-26 21:47:22 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/form-upload.js
@@ -0,0 +1,140 @@
+import { isEscapeKey, checkingMaxStrLength } from './util.js';
+import { scaleControlValue, closeScaleControlValue, changeEffect, initEffects, effectList, pictureUploadPreviewElement } from './slider.js';
+import { sendData } from './api.js';
+import { showAlert } from './util.js';
+import{showMessageSuccess,showMessageError} from './messages.js';
+const uploadFile=document.querySelector('#upload-file');
+const imgUploadOverlay=document.querySelector('.img-upload__overlay');
+const bodyElement=document.querySelector('body');
+const uploadCancel=document.querySelector('#upload-cancel');
+const imgUploadForm=document.querySelector('.img-upload__form');
+const hashTagsElement=document.querySelector('.text__hashtags');
+const descriptionElement=document.querySelector('.text__description');
+const MAX_HASHTAGS=5;
+const MAX_SYMBOLS=140;
+const submitButton = document.querySelector('.img-upload__submit');
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикую...';
+};
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFormEditImg();
+  }
+};
+
+
+function openUploadImg () {
+  document.addEventListener('keydown', onPopupEscKeydown);
+}
+
+function closeFormEditImg () {
+  imgUploadOverlay.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.imgUploadForm.reset();
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  closeScaleControlValue();
+
+  effectList.removeEventListener('change', changeEffect);
+  pictureUploadPreviewElement.removeAttribute('class');
+  pictureUploadPreviewElement.removeAttribute('style');
+}
+
+uploadCancel.addEventListener('click',()=>{
+  closeFormEditImg();
+});
+
+uploadFile.addEventListener('change', ()=>{
+  openUploadImg();
+  const sliderWrapper = document.querySelector('.effect-level');
+  imgUploadOverlay.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  initEffects();
+  scaleControlValue();
+  effectList.addEventListener('change', changeEffect);
+  sliderWrapper.classList.add('hidden');
+});
+
+//отменить обработчик Esc при фокусе
+hashTagsElement.addEventListener('keydown', (evt) =>  {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+});
+descriptionElement.addEventListener('keydown', (evt) =>  {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+});
+
+// Валидация хэштегов и комментариев
+const pristine = new Pristine(imgUploadForm,{
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
+
+const hashtags = function(value) {value.toLowerCase().split(' ');
+};
+
+pristine.addValidator(hashTagsElement,(value)=>hashtags(value).length <= MAX_HASHTAGS,
+  'нельзя указать больше пяти хэш-тегов');
+
+function isHashtagValid (value) {
+  const RegExp = /^#[A-Za-z0-9А-Яа-яЁё]{1,19}$/;
+  const array = value.split(' ');
+  for (const arrayElement of array) {
+    if (!RegExp.test(arrayElement) && arrayElement !== '') {
+      return false;
+    }
+  } return true;
+}
+pristine.addValidator(hashTagsElement,
+  (value) => isHashtagValid(value),
+  'хэш-тег начинается с символа # состоит из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.)'
+);
+
+pristine.addValidator(descriptionElement,
+  (value) => checkingMaxStrLength(value, MAX_SYMBOLS),
+  'длина комментария не больше 140 символов'
+);
+
+const onSuccess = () => {
+  closeFormEditImg();
+  unblockSubmitButton();
+  showMessageSuccess();
+};
+const onError = () => {
+  showAlert('Не удалось отправить форму. Попробуйте ещё раз');
+  unblockSubmitButton();
+  showMessageError();
+};
+
+const setFileFormSubmit = () => {
+  imgUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+        },
+        () => {
+          onError();
+        },
+        new FormData(evt.target),
+      );
+    }
+  });
+};
+
+export {setFileFormSubmit, closeFormEditImg};
+
---
+++ 10/js/util.js
@@ -0,0 +1,53 @@
+const ALERT_SHOW_TIME=5000;
+
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const returnRandomInteger = function (min, max) {
+  if(min>=0 && max>=0 && min<max && min!==max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return -1;
+};
+returnRandomInteger(1,78);
+
+//Функция для проверки максимальной длины строки
+const checkingMaxStrLength = function (str, max) {
+  return str.length<=max;
+};
+checkingMaxStrLength('Василий',456);
+
+
+//Функция проверки нажатия клавиши Escape
+const isEscapeKey = function(evt) {
+  return evt.key === 'Escape';
+};
+export{isEscapeKey,returnRandomInteger,checkingMaxStrLength};
+
+//ошибка вывода изображения с сервера
+const showAlert=(message)=>{
+  const alert=document.createElement('div');
+  alert.style.position='absolute';
+  alert.style.zIndex='100';
+  alert.style.left='0';
+  alert.style.top='0';
+  alert.style.right='0';
+  alert.style.padding='10px 3px';
+  alert.style.fontSize='30px';
+  alert.style.textAlign='center';
+  alert.style.backgroundColor='red';
+  alert.textContent=message;
+  document.body.append(alert);
+  setTimeout(()=>{
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export{showAlert};
+
+/*const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+export{debounce};*/
